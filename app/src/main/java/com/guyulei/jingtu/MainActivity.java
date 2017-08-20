08761e0716417d6f8b9ae7fa8636a6c51f888954
guyulei <1253908433@qq.com>
2017-08-21 06:59:45 +0800

    gu

--- app/src/main/java/com/guyulei/jingtu/MainActivity.java
+++ app/src/main/java/com/guyulei/jingtu/MainActivity.java
@@ -1,13 +1,45 @@
 package com.guyulei.jingtu;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar      mToolbar;
+    @BindView(R.id.drawerLayout)
+    DrawerLayout mDrawerLayout;//侧边 布局android:layout_gravity="left"  需自己打出
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initview();
     }
+
+    private void initview() {
+        setSupportActionBar(mToolbar);
+        ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+            supportActionBar.setHomeAsUpIndicator(R.mipmap.ic_launcher_round);
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar,menu);
+        return true;
+    }
+
+
 }
